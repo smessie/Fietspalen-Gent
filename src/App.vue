d7c1bfbab47877a9315d9fcd78dee0e0cfe825ed
smessie <smessie@smessie.com>
2022-04-25 15:23:02 +0200

    feat: Load datasets async and add loading animation (closes #18)

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,13 @@
         <h1>Fietsend Gent, een leugen of toch wel frequent?</h1>
         <h4>Freya Van Speybroeck, Thor Dossche & Ieben Smessaert</h4>
       </el-header>
-      <el-main>
+      <el-main v-if="!loaded">
+        <div class="loadingio-spinner-wedges-uak1yzl8zv"><div class="ldio-wtj2n53k5w">
+            <div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div></div>
+        </div></div>
+        <p>De data wordt momenteel ingeladen...</p>
+      </el-main>
+      <el-main v-if="loaded">
         <el-tabs v-model="activeName" class="demo-tabs">
           <el-tab-pane label="Fietspalen" name="first">
             <h3>Map</h3>
@@ -37,7 +43,7 @@ import DailyLineGraph from './components/DailyLineGraph.vue'
 import BarchartsContainer from './components/BarchartsContainer.vue'
 import Map from './components/Map.vue'
 import YearData from './components/YearData.vue'
-import { getDatasets } from '/src/js/bicycling-data.js';
+import { getDatasets, load } from '/src/js/bicycling-data.js';
 import { ref } from 'vue'
 
 export default {
@@ -52,7 +58,8 @@ export default {
     return {
       selectedStation: null,
       datasets: getDatasets(),
-      activeName: ref('first')
+      activeName: ref('first'),
+      loaded: false
     }
   },
   methods: {
@@ -62,6 +69,11 @@ export default {
     changeTab() {
       return
     }
+  },
+  mounted() {
+    load().then(() => {
+      this.loaded = true;
+    })
   }
 }
 </script>
@@ -86,4 +98,64 @@ export default {
   font-size: 32px;
   font-weight: 600;
 }
+
+/* LOADING ANIMATION */
+@keyframes ldio-wtj2n53k5w {
+  0% { transform: rotate(0deg) }
+  100% { transform: rotate(360deg) }
+}
+.ldio-wtj2n53k5w > div > div {
+  transform-origin: 100px 100px;
+  animation: ldio-wtj2n53k5w 3.0303030303030303s linear infinite;
+  opacity: 0.8
+}
+.ldio-wtj2n53k5w > div > div > div {
+  position: absolute;
+  left: 30px;
+  top: 30px;
+  width: 70px;
+  height: 70px;
+  border-radius: 70px 0 0 0;
+  transform-origin: 100px 100px
+}.ldio-wtj2n53k5w > div div:nth-child(1) {
+   animation-duration: 0.7575757575757576s
+ }
+.ldio-wtj2n53k5w > div div:nth-child(1) > div {
+  background: #e15b64;
+  transform: rotate(0deg);
+}.ldio-wtj2n53k5w > div div:nth-child(2) {
+   animation-duration: 1.0101010101010102s
+ }
+.ldio-wtj2n53k5w > div div:nth-child(2) > div {
+  background: #f47e60;
+  transform: rotate(0deg);
+}.ldio-wtj2n53k5w > div div:nth-child(3) {
+   animation-duration: 1.5151515151515151s
+ }
+.ldio-wtj2n53k5w > div div:nth-child(3) > div {
+  background: #f8b26a;
+  transform: rotate(0deg);
+}.ldio-wtj2n53k5w > div div:nth-child(4) {
+   animation-duration: 3.0303030303030303s
+ }
+.ldio-wtj2n53k5w > div div:nth-child(4) > div {
+  background: #abbd81;
+  transform: rotate(0deg);
+}
+.loadingio-spinner-wedges-uak1yzl8zv {
+  width: 200px;
+  height: 200px;
+  display: inline-block;
+  overflow: hidden;
+  background: #ffffff;
+}
+.ldio-wtj2n53k5w {
+  width: 100%;
+  height: 100%;
+  position: relative;
+  transform: translateZ(0) scale(1);
+  backface-visibility: hidden;
+  transform-origin: 0 0; /* see note above */
+}
+.ldio-wtj2n53k5w div { box-sizing: content-box; }
 </style>
--- src/js/bicycling-data.js
+++ src/js/bicycling-data.js
@@ -5,52 +5,26 @@
  */
 import { assert } from '@vue/compiler-core';
 import * as d3 from 'd3';
-import bataviabrug2018 from '../assets/datasets/fietstelpaal-bataviabrug-2018-gent.json';
-import bataviabrug2019 from '../assets/datasets/fietstelpaal-bataviabrug-2019-gent.json';
-import bataviabrug2020 from '../assets/datasets/fietstelpaal-bataviabrug-2020-gent.json';
-import bataviabrug2021 from '../assets/datasets/fietstelpaal-bataviabrug-2021-gent.json';
-import bijlokekaai2021 from '../assets/datasets/fietstelpaal-bijlokekaai-2021-gent.json';
-import coupureLinks2021 from '../assets/datasets/fietstelpaal-coupure-links-2021-gent.json';
-import dampoortZuid2018 from '../assets/datasets/fietstelpaal-dampoort-zuid-2018-gent.json';
-import dampoortZuid2019 from '../assets/datasets/fietstelpaal-dampoort-zuid-2019-gent.json';
-import dampoortZuid2020 from '../assets/datasets/fietstelpaal-dampoort-zuid-2020-gent.json';
-import dampoortZuid2021 from '../assets/datasets/fietstelpaal-dampoort-zuid-2021-gent.json';
-import gaardeniersbrug2020 from '../assets/datasets/fietstelpaal-gaardeniersbrug-2020-gent.json';
-import gaardeniersbrug2021 from '../assets/datasets/fietstelpaal-gaardeniersbrug-2021-gent.json';
-import groendreef2018 from '../assets/datasets/fietstelpaal-groendreef-2018-gent.json';
-import groendreef2019 from '../assets/datasets/fietstelpaal-groendreef-2019-gent.json';
-import groendreef2020 from '../assets/datasets/fietstelpaal-groendreef-2020-gent.json';
-import groendreef2021 from '../assets/datasets/fietstelpaal-groendreef-2021-gent.json';
-import zuidparklaan2018 from '../assets/datasets/fietstelpaal-zuidparklaan-2018-gent.json';
-import zuidparklaan2019 from '../assets/datasets/fietstelpaal-zuidparklaan-2019-gent.json';
-import zuidparklaan2020 from '../assets/datasets/fietstelpaal-zuidparklaan-2020-gent.json';
-import zuidparklaan2021 from '../assets/datasets/fietstelpaal-zuidparklaan-2021-gent.json';
-import fietspalen  from '../assets/datasets/fietstelpalen-gent.json';
+import fietspalen from '../../public/datasets/fietstelpalen-gent.json';
 
 
 export const data = {
-  'bataviabrug-2018': bataviabrug2018,
-  'bataviabrug-2019': bataviabrug2019,
-  'bataviabrug-2020': bataviabrug2020,
-  'bataviabrug-2021': bataviabrug2021,
-  'bijlokekaai-2021': bijlokekaai2021,
-  'coupure-links-2021': coupureLinks2021,
-  'dampoort-zuid-2018': dampoortZuid2018,
-  'dampoort-zuid-2019': dampoortZuid2019,
-  'dampoort-zuid-2020': dampoortZuid2020,
-  'dampoort-zuid-2021': dampoortZuid2021,
-  'gaardeniersbrug-2020': gaardeniersbrug2020,
-  'gaardeniersbrug-2021': gaardeniersbrug2021,
-  'groendreef-2018': groendreef2018,
-  'groendreef-2019': groendreef2019,
-  'groendreef-2020': groendreef2020,
-  'groendreef-2021': groendreef2021,
-  'zuidparklaan-2018': zuidparklaan2018,
-  'zuidparklaan-2019': zuidparklaan2019,
-  'zuidparklaan-2020': zuidparklaan2020,
-  'zuidparklaan-2021': zuidparklaan2021,
   'fietspalen': fietspalen,
 };
+
+export async function load() {
+  const datasets = [
+    'bataviabrug-2018', 'bataviabrug-2019', 'bataviabrug-2020', 'bataviabrug-2021', 'bijlokekaai-2021', 'coupure-links-2021',
+    'dampoort-zuid-2018', 'dampoort-zuid-2019', 'dampoort-zuid-2020', 'dampoort-zuid-2021', 'gaardeniersbrug-2020',
+    'gaardeniersbrug-2021', 'groendreef-2018', 'groendreef-2019', 'groendreef-2020', 'groendreef-2021', 'zuidparklaan-2018',
+    'zuidparklaan-2019', 'zuidparklaan-2020', 'zuidparklaan-2021'
+  ];
+  for (let dataset of datasets) {
+    const res = await fetch(`./datasets/fietstelpaal-${dataset}-gent.json`);
+    data[dataset] = await res.json();
+  }
+  return true;
+}
 
 export const unavailable = [
    'Visserij',
@@ -214,10 +188,10 @@ export function getDatasets() {
 }
 
 
-export function calculateDailyAverages(datasets){
-  let weekdays = ["Zondag","Maandag","Dinsdag","Woensdag","Donderdag","Vrijdag","Zaterdag"];
-  let total  = { Zondag: 0, Maandag: 0, Dinsdag: 0, Woensdag: 0, Donderdag: 0, Vrijdag: 0, Zaterdag:0}; 
-  let counts = { Zondag: 0, Maandag: 0, Dinsdag: 0, Woensdag: 0, Donderdag: 0, Vrijdag: 0, Zaterdag:0}; 
+export function calculateDailyAverages(datasets) {
+  let weekdays = ['Zondag', 'Maandag', 'Dinsdag', 'Woensdag', 'Donderdag', 'Vrijdag', 'Zaterdag'];
+  let total = {Zondag: 0, Maandag: 0, Dinsdag: 0, Woensdag: 0, Donderdag: 0, Vrijdag: 0, Zaterdag: 0};
+  let counts = {Zondag: 0, Maandag: 0, Dinsdag: 0, Woensdag: 0, Donderdag: 0, Vrijdag: 0, Zaterdag: 0};
 
   datasets.forEach(data => {
     let previous;
