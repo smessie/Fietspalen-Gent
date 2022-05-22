4bba324d5da3489956463223f18261b5d100e651
smessie <smessie@smessie.com>
2022-05-22 16:58:18 +0200

    feat: Add percentage of total to pie charts

--- src/components/YearTotalPieChart.vue
+++ src/components/YearTotalPieChart.vue
@@ -20,7 +20,18 @@ export default {
       const chart = {
         $schema: 'https://vega.github.io/schema/vega-lite/v5.json',
         data: {values: this.$props.data},
-
+        transform: [{
+          window: [{
+            op: 'sum',
+            field: 'amount',
+            as: 'total'
+          }],
+          frame: [null, null]
+        },
+          {
+            calculate: 'round(datum.amount/datum.total * 10000) / 100',
+            as: 'PercentOfTotal'
+          }],
         encoding: {
           theta: {field: 'amount', type: 'quantitative', stack: true},
           color: {
@@ -30,19 +41,20 @@ export default {
             scale: {
               range: ['#4c78a8', '#fcbc7c', '#54a44c', '#9cccec', '#8cd47c', '#f4841c', '#f2cf5b'],
               domain: [
-                  'Bataviabrug',
-                  'Zuidparklaan',
-                  'Groendreef',
-                  'Bijlokekaai',
-                  'Gaardeniersbrug',
-                  'Coupure Links',
-                  'Dampoort-Zuid'
+                'Bataviabrug',
+                'Zuidparklaan',
+                'Groendreef',
+                'Bijlokekaai',
+                'Gaardeniersbrug',
+                'Coupure Links',
+                'Dampoort-Zuid'
               ]
             },
           },
           tooltip: [
             {'field': 'name', 'type': 'nominal', 'title': 'Locatie'},
-            {'field': 'amount', 'type': 'quantitative', 'title': 'Fietsers'}]
+            {'field': 'amount', 'type': 'quantitative', 'title': 'Fietsers'},
+            {'field': 'PercentOfTotal', 'type': 'quantitative', 'title': '% van totaal'}]
         },
         layer: [
           {
